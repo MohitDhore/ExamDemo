9c89d274d2483be37e13fa3a90f358b9fd53fe93
Mohit Dhore <dhoremohit@gmail.com>
2023-02-02 07:13:54 +0530

    add msg to sum

--- sum.java
+++ sum.java
@@ -1,9 +1,9 @@
-class Sum{
+class Sum {
     public static void main(String[] args) {
         int a = 5;
         int b = 15;
 
-        System.out.println(a+b);
-        
-    } 
+        System.out.println("The SUM is : " + (a + b));
+
+    }
 }
